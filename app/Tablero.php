f80f915b1ea8f489a4e90f9e0e0067e19b7dd828
Euge <eug.damonte@gmail.com>
2022-08-23 00:26:44 -0300

    Ahora puede borrarse el último movimiento

--- app/Tablero.php
+++ app/Tablero.php
@@ -16,10 +16,13 @@ class Tablero{
 		$columna = $columna - 1;
 		$this->tablero[$columna][$this->nivelPorColumna[$columna]] = $ficha;
 		$this->nivelPorColumna[$columna] ++;
+		$this->secuencia[] = $columna
 	}
 	
 	public function deshacer_ultimo_movimiento(){
-		
+		$columna = array_pop($this->secuencia)
+		$this->tablero[$columna][$this->nivelPorColumna[$columna]] = NULL;
+		$this->nivelPorColumna[$columna] --;
 	}
 	
 }
