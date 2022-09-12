bdc4a26b3156bd640c491d2182b5f8a7b13ef759
Euge <eug.damonte@gmail.com>
2022-09-12 15:58:49 -0300

    Se añade una excepción

--- app/ValidadorDeSecuencia.php
+++ app/ValidadorDeSecuencia.php
@@ -1,17 +1,11 @@
 <?php
 
 namespace App;
-namespace App\Exceptions;
-use Exception;
-
-class ColumnaInvalida extends Exception {}
 
 class ValidadorDeSecuencia{
 	public function validarColumna($columna){
 		if($columna > 7 || $columna < 1){
 			throw new ColumnaInvalida("El número de columna no está admitido. Ingrese uno entre el 1 y el 7.");
 		}
-    return $columna;
 	}
 }
-
